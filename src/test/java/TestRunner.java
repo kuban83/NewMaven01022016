1f286d42e184896cf2923202c8681d878783e7e9
kuban83 <kuban83@inbox.ru>
2016-03-01 20:51:51 +0200

    Add Webdriver driver instance

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,5 +1,13 @@
+import org.openqa.selenium.WebDriver;
+
 /**
  * Created by Анна on 01.03.2016.
  */
 public class TestRunner {
+    // клонировать проект
+//    из файл нью from проджект версион контроль
+//
+            //
+    WebDriver driver;
+
 }
